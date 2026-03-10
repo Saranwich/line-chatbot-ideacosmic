7f859af2b201128caaf0ac9b4d43e6fdb7bb44b0
Saranwich <saranwichpochai.idea@gmail.com>
2026-03-10 21:23:21 +0700

    feat: setup database engine and get_session

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -1,1 +1,8 @@
-# Database connection management
+from sqlmodel import Session, create_engine
+from app import config
+
+engine = create_engine(config.DATABASE_URL, echo=True)
+
+def get_session ():
+    with Session(engine) as session :
+        yield session
