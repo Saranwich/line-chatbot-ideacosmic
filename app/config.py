0487bf1396c8508fb6d188a937dc67c780d8a97c
Saranwich <saranwichpochai.idea@gmail.com>
2026-03-10 21:14:53 +0700

    feat(database): get database_url from .env

--- app/config.py
+++ app/config.py
@@ -1,5 +1,11 @@
-# Settings and environment variables
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 LINE_CHANNEL_SECRET = os.getenv('LINE_CHANNEL_SECRET')
 LINE_CHANNEL_ACCESS_TOKEN = os.getenv('LINE_CHANNEL_ACCESS_TOKEN')
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL :
+    print("DATABASE_URL NOT FOUND in .env")
