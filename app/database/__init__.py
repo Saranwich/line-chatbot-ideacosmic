3c2c62bcb738706e0a7cd4633b60194a73a213ef
Saranwich <saranwichpochai.idea@gmail.com>
2026-03-10 22:31:27 +0700

    feat: setup init_db

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -1,7 +1,13 @@
-from sqlmodel import Session, create_engine
+from sqlmodel import Session, create_engine, SQLModel
 from app import config
+from app.models.reminder import Reminder
+from app.models.task import Task
+from app.models.user import User
 
 engine = create_engine(config.DATABASE_URL, echo=True)
+
+def init_db():
+    SQLModel.metadata.create_all(engine)
 
 def get_session ():
     with Session(engine) as session :
